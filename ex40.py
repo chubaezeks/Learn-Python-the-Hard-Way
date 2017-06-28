37584b13ccf82b8318702f57c950ee47c27f7d6d
chubaezeks <ezekschuba@gmail.com>
2017-06-28 06:31:51 -0700

    Added comments explaining the codes

--- ex40.py
+++ ex40.py
@@ -1,14 +1,17 @@
 
 #A first class example
+#We create a class this way.
 class Song(object):
 
+#a class can't work without the initialiser
     def __init__(self, lyrics):
         self.lyrics = lyrics
-
+#we define another function has a for loop with the previous function and prints out the result
     def sing_me_a_song(self):
         for line in self.lyrics:
             print line
 
+#These are the scripts wrapped up within these objects.
 happy_bday = Song(["Happy birthday to you",
                     "I don't want to get sued",
                     "So I'll stop right there"])
