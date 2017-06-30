99b380d85a8fd61ff4eca638b181011c47f80eb6
chubaezeks <ezekschuba@gmail.com>
2017-06-29 19:37:32 -0700

    Phase 2

--- ex41.py
+++ ex41.py
@@ -12,7 +12,12 @@ PHRASES = {
             "Make a class named %%% tha is-a %%%.",
         "class %%%(object):\n\tdef __init__(self,***)":
             "class %%% has-a __init__ that takes self and *** parameters.",
-        
-
-
+        "class %%%(object):\n\tdef ***(self,@@@)":
+            "class %%% has-a function named *** that takes self and @@@ parameters.",
+        "*** = %%%()":
+            "Set *** to an instance of class %%%.",
+        "***.***(@@@)":
+            "From *** get the function, and call it with parameters self, @@@.",
+        "***.*** = '***'":
+            "From *** get the *** attribute and set it to '***'."
 }
