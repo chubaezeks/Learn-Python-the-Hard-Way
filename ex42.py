93b0fb09a2683fec2a9770825add05ef92cf2aa0
chubaezeks <ezekschuba@gmail.com>
2017-07-07 21:03:20 +0100

    Answered the assignment

--- ex42.py
+++ ex42.py
@@ -2,75 +2,75 @@
 class Animal(object):
 pass
 
-## Dog is an animal
+## Class Dog is-a animal
 class Dog(Animal):
 
 def __init__(self, name):
-    ## self has a name that is a name
+    ## Class Dog has-a init that takes self and name parameters
 self.name = name
 
-## ??
+## Class Cat is-a animal
 class Cat(Animal):
 
 def __init__(self, name):
-    ## ??
+    ## Class cat has-a init that takes self and name parameters
 self.name = name
 
-## ??
+## Class Person is-a object
 class Person(object):
 
     def __init__(self, name):
-        ## ??
+        ## Class person has-a init that takes self and name
         self.name = name
 
         ## Person has-a pet of some kind
         self.pet = None
 
-## ??
+## Class employee is a person
 class Employee(Person):
 
     def __init__(self, name, salary):
-    ## ?? hmm what is this strange magic?
+    ## class employee is a person that takes a self, name and salary parameters hmm what is this strange magic?
     super(Employee, self).__init__(name)
-    ## ??
+    ## super is-a employee that has-a init
     self.salary = salary
 
 
-## ??
+## class fish is an object
 class Fish(object):
     pass
 
-## ??
+## class Salmon is-a fish
 class Salmon(Fish):
     pass
 
-## ??
+## Class Halibut is-a Fish
 class Halibut(Fish):
     pass
 
 ## rover is-a Dog
 rover = Dog("Rover")
 
-## ??
+## satan is-a Cat
 satan = Cat("Satan")
 
-## ??
+## mary is-a person
 mary = Person("Mary")
 
-## ??
+## mary has-a pet that is-a satan
 mary.pet = satan
 
-## ??
+## frank is-a employee
 frank = Employee("Frank", 120000)
 
-## ??
+## frant has-a pet that is a rover
 frank.pet = rover
 
-## ??
+##flipper is-a fish
 flipper = Fish()
 
-## ??
+## crouse is-a salmon
 crouse = Salmon()
 
-## ??
+## harry is-a Halibut
 harry = Halibut()
