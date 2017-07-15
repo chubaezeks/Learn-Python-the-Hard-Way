c7366c8d563b82a45b2b862a664e475d43b30ddc
chubaezeks <ezekschuba@gmail.com>
2017-07-15 06:10:59 +0100

    Added more comments

--- ex43.py
+++ ex43.py
@@ -31,6 +31,7 @@ class Engine(object):
             #be sure to print out the last scene
             current_scene.enter()
 
+#This is the first scene we create
 #Get scene from class death
 class Death(Scene):
 
@@ -46,6 +47,7 @@ class Death(Scene):
         print Death.quips[randint(0, len(self.quips)-1)]
         exit(1)
 
+#create corridor scene with a if-elif-else scenarios
 class CentralCorridor(Scene):
 
     def enter(self):
@@ -86,7 +88,7 @@ class CentralCorridor(Scene):
             return 'central_corridor'
 
 
-
+#Make the task to get the bomb interesting by introducing a guess game
 class laserWeaponAmory(Scene):
 
     def enter(self):
@@ -97,10 +99,14 @@ class laserWeaponAmory(Scene):
         print "and you need the code to get the bomb out. If you get the code"
         print "wrong 10 times then the lock closes forever and you can't"
         print "get the bomb. The code is 3 digits."
+        #use that module randint to create a randomised code
         code = "%d%d%d" % (rand(1,9), randint(1,9), randint(1,9))
         guess = raw_input("[keypad]>")
+        #start guesses at 0, so subsequent can follow after
         guesses = 0
 
+        #I had no idea you could run several conditions in a loop!
+        #so here's a while loop for when the guess isn't correct and less than 10
         while guess != code and guesses < 10:
             print "BZZZZED!"
             guesses = +1
@@ -196,6 +202,7 @@ class Finished(Scene):
 
 class Map(object):
 
+#In the map, the scene classes are all placed in a dictionary
     scenes = {
     'central_corridor': CentralCorridor(),
     'laser_weapon_amory': laserWeaponAmory(),
@@ -208,6 +215,7 @@ class Map(object):
     def __init__(self, start_scene):
         self.start_scene = start_scene
 
+    #can't say I get why this is done
     def next_scene(self, scene_name):
         val = Map.scenes.get(scene_name)
         return val
