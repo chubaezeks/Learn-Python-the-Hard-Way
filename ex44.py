58f6011da45f95ff006c83f644aa9240b24b1c50
chubaezeks <ezekschuba@gmail.com>
2017-07-16 13:38:52 +0100

    Override and Alter version of Inheritance. Composition instead of inheritance.

--- ex44.py
+++ ex44.py
@@ -49,3 +49,94 @@ son = Child()
 
 dad.override()
 son.override()
+
+#alter before or alter after
+class Parent(object):
+    def altered(self):
+        print "PARENT altered()"
+
+class Child(Parent):
+
+    def altered(self):
+        print "CHILD, BEFORE PARENT altered()"
+
+        #call super with arguments Child and self. Then call the function altered on whatever it returns
+        super(Child,self).altered()
+        print "CHILD, AFTER PARENT altered()"
+
+dad = Parent()
+son = Child()
+
+dad.altered()
+son.altered()
+
+
+#All three combined
+class Parent(object):
+
+    def override(self):
+        print "PARENT override()"
+
+    def implicit(self):
+        print "PARENT implicit()"
+
+    def altered(self):
+        print "PARENT altered()"
+
+class Child(Parent):
+
+    def override(self):
+        print "CHILD override()"
+
+    def altered(self):
+        print "CHILD BEFORE PARENT altered()"
+        super(Child, self).altered()
+        print "CHILD, AFTER PARENT altered()"
+
+dad = Parent()
+son = Child()
+
+dad.implicit()
+son.implicit()
+
+dad.override()
+son.override()
+
+dad.altered()
+son.altered()
+
+#Composition
+class Other(object):
+
+    def override(self):
+        print "OTHER override()"
+
+    def implicit(self):
+        print "OTHER implicit()"
+
+    def altered(self):
+        print "OTHER altered()"
+
+Class Child(object):
+
+def __init__(self):
+    self.other = Other()
+
+def implicit(self):
+    self.other = implicit()
+
+def override(self):
+    print "CHILD override()"
+
+def altered(self):
+    print "CHILD BEFORE OTHER altered()"
+    self.other.altered()
+    print "CHILD, AFTER OTHER altered()"
+
+
+
+son = Child()
+
+son.implicit()
+son.override()
+son.altered()
