7698865144444a748a5d586ff97b47c7591b7639
chubaezeks <ezekschuba@gmail.com>
2017-07-22 22:41:08 +0100

    Storyline for Game

--- ex45.py
+++ ex45.py
@@ -1,5 +1,4 @@
-# create a game that chronicles the journey from Abuja to Lagos. 
-# the scenes should be each place we've been in. the park, outskirt, bus stop, bad road
-# the actions include choosing our seats, then deciding to sleep or read,
-# then leaving the bus and deciding whether to buy food or not
-#
+# Start at Winterfell
+# Jon Snow joins the Night's Watch
+# Jon joins the wildlings
+# Jon betrays the wildlings
