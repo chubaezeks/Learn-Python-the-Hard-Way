8a70c30a9d3cacc2a698ea3401539a3387486080
chubaezeks <ezekschuba@gmail.com>
2017-07-26 18:29:38 +0100

    Framework for simple Game of Thrones game

--- ex45.py
+++ ex45.py
@@ -1,4 +1,37 @@
-# Start at Winterfell
-# Jon Snow joins the Night's Watch
-# Jon joins the wildlings
-# Jon betrays the wildlings
+
+class Scene(object):
+    def enter(self):
+        pass
+
+class Engine(object):
+    def __init__(self, scene_map):
+        pass
+
+class Death(Scene):
+    pass
+
+class WesterosRoad(Scene):
+    pass
+
+class Winterfell(Scene):
+    pass
+
+class IronIslands(Scene):
+    pass
+
+class KingsLanding(Scene):
+    pass
+
+class Map(object):
+    def __init__(self, start_scene):
+        pass
+
+    def next_scene(self, scene_name):
+        pass
+
+    def opening_scene(self):
+        pass
+
+a_map = Map('WesterosRoad')
+a_game = Engine(a_map)
+a_game.play()
