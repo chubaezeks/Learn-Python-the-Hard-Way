1825ba110a1c3b1dd929788a32ce4f7e312f1824
chubaezeks <ezekschuba@gmail.com>
2017-07-27 10:54:51 +0100

    Created the Death Scene for the game

--- ex45.py
+++ ex45.py
@@ -1,14 +1,36 @@
+from sys import exit
+from random import randint
 
 class Scene(object):
     def enter(self):
-        pass
+        print "This scene has not been configured. Subclass and implement enter()"
+        exit(1)
 
 class Engine(object):
     def __init__(self, scene_map):
-        pass
+        self.scene_map = scene_map
+
+    def play(self):
+        current_scene = self.scene_map.opening_scene()
+
+        while True:
+            print "n\-----"
+            next_scene_name = current_scene.enter()
+            current_scene = self.scene_map.next_scene(next_scene_name)
 
 class Death(Scene):
-    pass
+
+    # create a list with words to be said when the character dies
+    quips = [
+        "Vaga molorgus. All men must die...but gosh...you really deserved to die.",
+        "Great job. Now you're one of the thousands that have perished in this war.",
+        "Westeros is certainly better off now that you're gone."
+        "Heard the white walkers are recruiting members. You might be more useful there."
+    ]
+
+    def enter(self):
+        print Death.quips[randint(0, len(self.quips)-1)]
+        exit(1)
 
 class WesterosRoad(Scene):
     pass
